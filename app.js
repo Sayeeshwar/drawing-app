d709bf96f50a1e68611f7a61ff9615b0dd686571
Sayeeshwar <sayeeshwar.girish@gmail.com>
2019-06-26 15:28:33 +0530

    Add functions to start and stop pen

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ window.addEventListener('load',()=>{
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
     let painting = false;
+
+    function startPosition(e){
+        painting=true;
+        ctx.beginPath();
+        draw(e);
+        
+    }
+    function finishPosition(){
+        painting=false;
+        ctx.endpath();
+    }
+
+
+    canvas.addEventListener('mousedown',startPosition);
+    canvas.addEventListener('mouseup',finishPosition);
     
 });
 
